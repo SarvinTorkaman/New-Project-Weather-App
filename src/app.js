13afb299c366bf2d1010cd045e64c753069a007a
SarvinTorkaman <88194904+SarvinTorkaman@users.noreply.github.com>
2021-09-25 10:04:47 +0330

    cleaned the code lines

--- src/app.js
+++ src/app.js
@@ -21,25 +21,16 @@ function displaytime(time) {
     hour
   )}:${timeformat(mins)}`;
 }
-//4444
 
 function formatday(time) {
   let date = new Date(time * 1000);
-
   let days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
-
   return days[date.getDay()];
 }
 function displayWeatherForecast(response) {
-  console.log(response);
-  console.log(response.data.daily);
-  //let days = [];
   let array = response.data.daily;
-  console.log(array.length);
-  // console.log(days);
   let forecastElement = document.querySelector("#forecast");
   let forecastHtml = `<div class="row  d-flex justify-content-center">`;
-
   array.forEach(function (forecastDay, index) {
     if (index < 6) {
       forecastHtml =
@@ -73,20 +64,17 @@ function displayWeatherForecast(response) {
     }
   });
   forecastHtml = forecastHtml + `</div>`;
-  // console.log(forecastHtml);
   forecastElement.innerHTML = forecastHtml;
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-
   console.log(apiUrl);
   axios.get(apiUrl).then(displayWeatherForecast);
 }
 function handleApiResponse(response) {
-  console.log(response);
+  //console.log(response);
   document.querySelector("#temp").innerHTML = Math.round(
     response.data.main.temp
   );
@@ -113,7 +101,6 @@ function handleApiResponse(response) {
 
   getForecast(response.data.coord);
 }
-//888
 
 function search(city) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
@@ -128,19 +115,10 @@ function handleSubmitButton(event) {
   search(cityname);
 
   document.getElementById("search-input").value = "";
-  document.querySelector("#celcius").classList.add("active");
-  document.querySelector("#fahrenheit").classList.remove("active");
 }
 
-function getresponse(response) {
-  console.log(response);
-}
 let celciustemp = null;
 search("babol");
 
 let submitForm = document.querySelector("#submit-form");
 submitForm.addEventListener("submit", handleSubmitButton);
-
-// let forecastAiKey = "de78e0e0e6d92ec1e8b40b7082dc0697";
-// let forecastUrl = `https://api.openweathermap.org/data/2.5/forecast/daily?q=paris&cnt=6&appid=dbf20d5c78580523bd2bd1f7ce5630d5&units=metric`;
-// axios.get(forecastUrl).then(getresponse);
